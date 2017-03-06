d23e68e44e0f135c5e1e57f519ba8133d5d683b2
Alessandro Di Lello <ale@lovethesales.com>
2017-03-06 23:58:07 +0000

    trigger build

--- DockerConsoleApp/Program.cs
+++ DockerConsoleApp/Program.cs
@@ -11,6 +11,7 @@ namespace DockerConsoleApp
         private string dockerApiKey = "0c99f6bb-0980-4f51-bcdc-fd774831b570";
         static void Main(string[] args)
         {
+            Console.WriteLine("Running from ACS");
             Console.WriteLine("Hello Docker World!");
         }
     }
